d597e2a44ddd521288c4cb49d15f6f155bc2ac49
Neemias Vieira <neemias.vieira@live.com>
2023-05-28 19:02:47 -0300

    Update cors.js

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,7 +1,8 @@
 const permission = (response) => {
   response.setHeader("Access-Control-Allow-Origin", "*")
   response.setHeader("Access-Control-Allow-Credentials", "true");
-  response.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+  response.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
+  response.setHeader("Access-Control-Allow-Headers", "*")
 }
 
 export {permission}
